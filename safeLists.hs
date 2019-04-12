ecdf95cfdd3e60790aed5cfb3dd55a0454cb318d
Seth Fogarty <sfogarty@trinity.edu>
2019-04-12 13:48:49 -0500

    push

--- safeLists.hs
+++ safeLists.hs
@@ -12,7 +12,7 @@ thirdElem lst =
     Nothing -> Nothing
     Just tail -> case mTail tail of
                    Nothing -> Nothing
-                   Just tail2 = mHead tail2
+                   Just tail2 -> mHead tail2
 
 thirdElemb lst = 
     do lst' <- mTail lst
@@ -21,4 +21,24 @@ thirdElemb lst =
 
 
 twiceFifth :: [Int] -> Maybe Int
-twiceFifth lst = 
+twiceFifth lst = mHead =<< mTail =<< mTail =<< mTail =<< mTail lst
+twiceFith lst = 
+    do tail <- mTail lst
+       ttail <- mTail tail
+       tttail <- mTail ttail
+       ttttail <- mTail tttail
+       h <- mHead ttttail
+       return (2*h)
+
+(=<) :: (a -> Maybe b) -> Maybe a -> Maybe b
+(=<) f mv = 
+    do v <- mv
+       f v
+
+twiceKth :: Int -> [Int] -> Maybe Int
+twiceKth 0 lst =
+    do h <- mHead lst
+       return (2*h)
+twiceKth k lst =
+    do xs <- mTail lst
+       twiceKth (k-1) xs
