bec7c1692ce23d6e74ef98ddac5e3da3759d4e1b
Seth Fogarty <sfogarty@trinity.edu>
2019-04-12 14:24:45 -0500

    push

--- Fortunes.hs
+++ Fortunes.hs
@@ -1,11 +1,23 @@
-main = do
-    fortuneText <- readFile "fortunes.txt"
-    let fortunes = lines fortuneText
-    putStr "What is your name? "
-    name <- getLine
-    putStr $ "Welcome " ++ name ++ ". Please give me a number: "
-    index <- getLine
-    putStrLn $ "Your fortune: " ++ fortunes `at` (read index) 
+main = do fortuneText <- readFile "fortunes.txt"
+          let fortunes = lines fortuneText
+          name <- prompt "What is your name? "
+          putStr $ "Welcome " ++ name ++ "."
+          tellFortune fortunes
+
+prompt :: String -> IO String
+prompt str = do
+  putStr str
+  anwer <- getLine
+  return anwer
+
+tellFortune :: [String] -> IO ()
+tellFortune fortunes = do
+  answer <- prompt "Do you want a fortune?"
+  if answer == "yes"
+  then do index <- prompt "Please give me a number: "
+          putStrLn $ "Your fortune: " ++ fortunes `at` (read index) 
+          tellFortune fortunes
+  else putStr "Goodbye."
 
 at :: [a] -> Int -> a
 --at lst index = lst !! (index `mod` (length lst))
