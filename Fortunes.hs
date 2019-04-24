d06b53bd60ed45f2c1c0d92c6b21c4da99b7a779
Seth Fogarty <sfogarty@trinity.edu>
2019-04-24 14:11:21 -0500

    push

--- Fortunes.hs
+++ Fortunes.hs
@@ -1,14 +1,36 @@
-main = do fortuneText <- readFile "fortunes.txt"
-          let fortunes = lines fortuneText
-          name <- prompt "What is your name? "
-          putStr $ "Welcome " ++ name ++ "."
-          tellFortune fortunes
+import System.Environment
+import System.IO
+import System.Console.GetOpt
+
+data Flag = Help | Start String | Count String deriving (Eq, Show)
+
+options :: [OptDescr Flag]
+options = [ Option ['h'] ["help", "why"] (NoArg Help) "Print a help message and exit."
+          , Option ['s'] ["start"] (ReqArg Start "<num>") "Print the <num> fortune and exit."
+          , Option ['k'] ["kount"] (ReqArg Count "<num>") "Print <num> fortunes each time."
+          ]
+
+main :: IO ()
+main = do args <- getArgs
+          let (flags, others, errors) = getOpt Permute options args
+          if Help `elem` flags || (not $ null errors)
+          then putStrLn $ usageInfo "Fortunes [OPTIONS...] [file]" options
+          else let fileName = if null others then "fortunes.txt" else head others
+               in do fortuneText <- readFile fileName
+                     let fortunes = lines fortuneText
+                     name <- prompt "What is your name? "
+                     putStr $ "Welcome " ++ name ++ ". "
+                     tellFortune fortunes
 
 prompt :: String -> IO String
 prompt str = do
   putStr str
+  hFlush stdout
   anwer <- getLine
   return anwer
+
+getBool :: IO Bool
+getBool = undefined
 
 tellFortune :: [String] -> IO ()
 tellFortune fortunes = do
